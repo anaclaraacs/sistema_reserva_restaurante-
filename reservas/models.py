96ed077e0811555149dfe3019bf7b477ec0a6454
Ana Clara <ana927742@gmail.com>
2024-12-03 23:18:20 -0300

    adicionando tabelas em models.py

--- reservas/models.py
+++ reservas/models.py
@@ -3,28 +3,39 @@ from django.forms import ValidationError
 from validate_docbr import CPF
 
 
+# Função para validar CPF
 def validar_cpf(value):
     cpf = CPF()
     if not cpf.validate(value):
-        raise ValidationError('Cpf inválido')
+        raise ValidationError('CPF inválido')
 
+
+# Modelo Cliente
 class Cliente(models.Model):
-    nome=models.CharField(max_length=100)
-    cpf=models.CharField(max_length=14,validators=[validar_cpf])
-    telefone=models.CharField(max_length=14)
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14, validators=[validar_cpf])
+    telefone = models.CharField(max_length=14)
+
+    def __str__(self):  # retorna o nome do cliente como uma string
+        return self.nome
 
 
-    def __str__(self):  # retorna o nome do cliente (representa o objeto como uma string)
-        return self.nome 
-    
-
+# Modelo Mesa
 class Mesa(models.Model):
-    numero=models.IntegerField(unique=True)
-    capacidade=models.IntegerField()
-    ocupada=models.BooleanField(default=False)
+    numero = models.IntegerField(unique=True)
+    capacidade = models.IntegerField()
+    ocupada = models.BooleanField(default=False)
 
     def __str__(self):
         return f"Mesa {self.numero} - {self.capacidade} pessoas"
-     
 
 
+# Modelo Reserva
+class Reserva(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    mesa = models.ForeignKey(Mesa, on_delete=models.CASCADE)
+    data_hora = models.DateTimeField()
+
+
+    def __str__(self):
+        return f"Reserva de {self.cliente.nome} para a Mesa {self.mesa.numero} em {self.data_hora.strftime('%d/%m/%Y %H:%M')}"

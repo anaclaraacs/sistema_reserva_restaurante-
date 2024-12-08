3e0e9f747b515d3a3abdc076c195c3ac5b0ec99f
Nayara Maria <dev.nayara@gmail.com>
2024-12-08 13:46:46 -0300

    Finalizando o template

--- setup/urls.py
+++ setup/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from reservas import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home_view, name='home'),
+    path('reserva/', views.reserva_view, name='reserva'),
+    path('perfil/', views.perfil_view, name='perfil'),
+    path('login/', views.login_view, name='login'),
+    path('cadastro/', views.cadastro_view, name='cadastro'),
 ]

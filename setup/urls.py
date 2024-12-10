7ebc67bce403f94512c8971b15d82b88be54409a
Nayara Maria <dev.nayara@gmail.com>
2024-12-10 15:00:56 -0300

    salvando alterações do view

--- setup/urls.py
+++ setup/urls.py
@@ -20,10 +20,10 @@ from reservas import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'),
-    path('reserva/', views.reserva_view, name='reserva'),
-    path('perfil/', views.perfil_view, name='perfil'),
-    path('login/', views.login_view, name='login'),
-    path('cadastro/', views.cadastro_view, name='cadastro'),
+    path('', views.home, name='home'),
+    path('reserva/', views.reserva, name='reserva'),
+    path('perfil/', views.perfil, name='perfil'),
+    path('login/', views.login, name='login'),
+    path('cadastro/', views.cadastro, name='cadastro'),
     path('fazer_reserva/', views.fazer_reserva, name='fazer_reserva'),
 ]

deb74762a519805397e3b2ead591bcc122569e37
Nayara Maria <dev.nayara@gmail.com>
2024-12-10 22:35:41 -0300

    Conectando página de cadastro e inicial login

--- reservas/forms.py
+++ reservas/forms.py
@@ -1,6 +1,22 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from .models import Cliente
 
-class FormEmail(AuthenticationForm):
-    #Aqui eu estou alterando o 'username' nativo do python para ter a formataçaõ de um email
-    username = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'autofocus': True}))
+class Cadastro(forms.ModelForm):
+    senha = forms.CharField(widget=forms.PasswordInput, max_length=255)
+
+    class Meta:
+        model = Cliente
+        fields = ['nome', 'telefone', 'email', 'senha']
+
+    def save(self, commit=True):
+        cliente = super().save(commit=False)
+        cliente.set_senha(self.cleaned_data['senha'])  # Criptografa a senha
+        if commit:
+            cliente.save()
+        return cliente
+    
+    from django import forms
+
+class Login(forms.Form):
+    email = forms.EmailField(max_length=255)
+    senha = forms.CharField(widget=forms.PasswordInput)

bba5d5d49573fd1488a01f5bc234308882bbfdad
Nayara Maria <dev.nayara@gmail.com>
2024-12-14 21:05:35 -0300

    CRUD finalizado

--- setup/urls.py
+++ setup/urls.py
@@ -5,10 +5,10 @@ from reservas import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    # path('reserva/', views.fazer_reserva, name='reserva'),
     path('perfil/', views.perfil, name='perfil'),
     path('login/', views.login, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('fazer_reserva/', views.fazer_reserva, name='fazer_reserva'),
+    path('excluir_reserva/', views.excluir_reserva, name='excluir_reserva'),
 ]
 

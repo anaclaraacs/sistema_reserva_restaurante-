8a5749e42086dfbc75fe84b9b916eb6c1ec58a9c
Nayara Maria <dev.nayara@gmail.com>
2025-02-11 22:28:44 -0300

    alterações no consumidor

--- notificador/notificador_consumidor.py
+++ notificador/notificador_consumidor.py
@@ -1,22 +1,36 @@
 import pika
 from time import sleep
+import logging
 
-#sleep(10)
 RABBITMQ_HOST = "rabbitmq"
 QUEUE_NAME = "reservas"
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
 def callback(ch, method, properties, body):
     mensagem = body.decode("utf-8")
-    print(f"📩 Mensagem recebida: {mensagem}")
+    logger.info(f"📩 Mensagem recebida: {mensagem}")
     # Aqui você pode adicionar a lógica para enviar notificações
 
+
 print(RABBITMQ_HOST)
-sleep(20)
-conexao = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+
+while True:
+    try:
+        conexao = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+        if conexao:
+            break
+    except Exception as e:
+        logger.error(e)
+
 canal = conexao.channel()
 canal.queue_declare(queue=QUEUE_NAME)
 
-print("🎧 Consumidor RabbitMQ rodando...")
+logger.info("🎧 Consumidor RabbitMQ rodando...")
 canal.basic_consume(queue=QUEUE_NAME, on_message_callback=callback, auto_ack=True)
 
 canal.start_consuming()
